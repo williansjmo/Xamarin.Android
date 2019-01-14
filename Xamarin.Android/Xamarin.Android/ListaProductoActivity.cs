9306114fd26d3723ffe2323ffb945d4334eee6a0
Willians Melendez <willian_melendez@hotmail.com>
2019-01-13 22:12:43 -0400

    Inicial

---
+++ Xamarin.Android/Xamarin.Android/ListaProductoActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace Xamarin.Android
+{
+    [Activity(Label = "ListaProductoActivity")]
+    public class ListaProductoActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            var items = JsonConvert.DeserializeObject<List<string[]>>(Intent.GetStringExtra("data"));
+
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                var layoutH = new LinearLayout(this);
+                layoutH.Orientation = Orientation.Horizontal;
+
+                var txtProducto = new TextView(this);
+                txtProducto.Text = items[i][0];
+                txtProducto.SetWidth(500);
+
+                var txtCantidad = new TextView(this);
+                txtCantidad.Text = items[i][1];
+                txtCantidad.SetWidth(500);
+
+                layoutH.AddView(txtProducto);
+                layoutH.AddView(txtCantidad);
+                layout.AddView(layoutH);
+            }
+            SetContentView(layout);
+        }
+    }
+}
---
+++ Xamarin.Android/Xamarin.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using Android.Content;
+using Newtonsoft.Json;
+
+namespace Xamarin.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public List<string[]> miProducto = new List<string[]>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            var btn = FindViewById<Button>(Resource.Id.btnAgregar);
+            btn.Click += btn_Click;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            var producto = FindViewById<EditText>(Resource.Id.txtProducto);
+            var cantidad = FindViewById<EditText>(Resource.Id.txtCantidad);
+            string[] data = new string[] { producto.Text,cantidad.Text };
+            miProducto.Add(data);
+
+            producto.Text = "";
+            cantidad.Text = "";
+
+            var intent = new Intent(this, typeof(ListaProductoActivity));
+            intent.PutExtra("data", JsonConvert.SerializeObject(miProducto));
+
+            StartActivity(intent);
+        }
+    }
+}
